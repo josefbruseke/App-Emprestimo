7af356553e9383a6a114894093f29e7c733d04f5
Josef Bruseke <josefbruseke@gmail.com>
2023-11-15 22:58:15 -0300

    add setters and getters

---
+++ model/amigo.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Amigo {
+    private String nome;
+    private String telefone;
+    private String cpf;
+
+    public void setNome(String nomeUsuario) {
+        nome = nomeUsuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setTelefone(String telefoneUsuario) {
+        telefone = telefoneUsuario;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setCpf(String cpfUsuario) {
+        cpf = cpfUsuario;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+}
+
+
