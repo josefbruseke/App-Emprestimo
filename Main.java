26f12ac10a06a22eb52ee68b75402bb443f69a29
Josef Bruseke <josefbruseke@gmail.com>
2023-12-02 23:06:34 -0300

    add main

---
+++ Main.java
@@ -0,0 +1,8 @@
+import controler.ControladorSistema;
+
+public class Main {
+    public static void main(String[] args) {
+        ControladorSistema controladorSistema = new ControladorSistema();
+        controladorSistema.inicializaSistema();
+    }
+}
