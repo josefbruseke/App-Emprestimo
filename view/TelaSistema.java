c36a89ea03b18733ebd8db034404df50af13a9ef
Josef Bruseke <josefbruseke@gmail.com>
2023-12-14 12:57:46 -0300

    fix method

--- view/TelaSistema.java
+++ view/TelaSistema.java
@@ -4,16 +4,21 @@ import java.util.Scanner;
 
 public class TelaSistema{
 
+    private final Scanner input;
+
+    public TelaSistema() {
+        this.input = new Scanner(System.in);
+    }
+    
     public int telaOpcoes(){
-        try (Scanner input = new Scanner(System.in)) {
-            System.out.println("------AMIGO DO LIVRO------");
-            System.out.println("1 - Livros");
-            System.out.println("2 - Amigos");
-            System.out.println("3 - Emprestimos");
-            System.out.println("0 - Finalizar Sistema");
-            System.out.print("Escolha a opção desejada: ");
-            int opcao = input.nextInt();
-            return opcao;
-        }
+        System.out.println("------AMIGO DO LIVRO------");
+        System.out.println("1 - Livros");
+        System.out.println("2 - Amigos");
+        System.out.println("3 - Emprestimos");
+        System.out.println("0 - Finalizar Sistema");
+        System.out.print("Escolha a opção desejada: ");
+        int opcao = this.input.nextInt();
+        this.input.nextLine();
+        return opcao;
     }
 }
