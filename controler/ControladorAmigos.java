46f94a8c5426598b0c1dee454b3b9c98352872ff
Josef Bruseke <josefbruseke@gmail.com>
2023-12-17 21:17:27 -0300

    add various changes

--- controler/ControladorAmigos.java
+++ controler/ControladorAmigos.java
@@ -1,22 +1,30 @@
 package controler;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import model.Amigo;
 import view.TelaAmigo;
 
 public class ControladorAmigos {
-    private Amigo amigo;
     private TelaAmigo telaAmigo;
     private ControladorSistema controladorSistema;
+    private ArrayList<Amigo> amigos;
 
     public ControladorAmigos(ControladorSistema controladorSistema) {
         this.controladorSistema = controladorSistema;
         this.telaAmigo = new TelaAmigo();
+        this.amigos = new ArrayList<>();
     }
 
 
     public void incluirAmigo() {
+        HashMap<String, String> dadosAmigo = this.telaAmigo.pegadaDadosAmigo();
+        String nome = dadosAmigo.get("nome");
+        String telefone = dadosAmigo.get("telefone");
+        String cpf = dadosAmigo.get("cpf");
+        Amigo novo_amigo = new Amigo(nome, telefone, cpf);
+        this.amigos.add(novo_amigo);
 
     }
 
@@ -25,15 +33,23 @@ public class ControladorAmigos {
     }
 
     public void listaAmigos() {
-
-    }
+        this.telaAmigo.imprimeCabecalho();
+        
+        for(Amigo amigo : amigos) {
+          this.telaAmigo.imprimeAmigo(
+            amigo.getNome(),
+            amigo.getTelefone(), 
+            amigo.getCpf()  
+          );
+        }
+      }
 
     public void excluirAmigo() {
 
     }
 
     public void retornar() {
-        
+        this.controladorSistema.abreTela();
     }
 
     public void abreTela() {
@@ -44,16 +60,16 @@ public class ControladorAmigos {
         listaOpcoes.put(4, this::excluirAmigo);
         listaOpcoes.put(0, this::retornar);
 
-        int opcao = this.telaAmigo.telaOpcoes();
+        while (true) {
+           int opcao = this.telaAmigo.telaOpcoes();
         if (listaOpcoes.containsKey(opcao)) {
             listaOpcoes.get(opcao).run();
         } else {
             System.out.println("Opção Inválida. Tente novamente");
             this.abreTela();
         }
-
     }
-
+    }
 
 }
 
