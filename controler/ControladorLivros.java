59d15a4a9ff7dd3ef558dd5c74d127f428255347
Josef Bruseke <josefbruseke@gmail.com>
2023-12-17 21:17:33 -0300

    add various changes

--- controler/ControladorLivros.java
+++ controler/ControladorLivros.java
@@ -2,6 +2,7 @@ package controler;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import model.Livro;
 import view.TelaLivro;
@@ -17,18 +18,41 @@ public class ControladorLivros {
         this.livros = new ArrayList<>();
     }
 
+
+    public Livro pega_livro_por_codigo(int codigo) {
+        for (Livro livro: this.livros) {
+            if (livro.getCodigo() == codigo) {
+                return livro;
+            }
+        }
+        return null;
+    }
+
+
     public void incluirLivro() {
-        Object[] dadosLivro = this.telaLivro.pega_dados_livro();
-        String titulo = (String) dadosLivro[0]; 
-        int codigo = (int) dadosLivro[1];
+        Map<String, Object> dadosLivro = this.telaLivro.pegaDadosLivro();
+        String titulo = (String) dadosLivro.get("titulo"); 
+        int codigo = (int) dadosLivro.get("codigo");
         Livro novo_livro = new Livro(titulo, codigo);
-        this.livros.add(novo_livro);
-        this.abreTela();        
+        this.livros.add(novo_livro);     
     }
 
     public void alterarLivro() {
+        this.listaLivro();
+        int codigo_livro = this.telaLivro.selecionaLivro();
+        Livro livro = this.pega_livro_por_codigo(codigo_livro);
 
+        if (livro != null) {
+        Map<String, Object> dadosLivro = this.telaLivro.pegaDadosLivro();
+            int novo_codigo = (int) dadosLivro.get("codigo");
+            String novo_titulo = (String) dadosLivro.get("titulo");
+            livro.setTitulo(novo_titulo);
+            livro.setCodigo(novo_codigo);
+        } else {
+            this.telaLivro.mostraMensagem("ATENÇÃO: Livro não existe!");
+        }
     }
+
 
     public void listaLivro() {
         System.out.println("Código      Titulo");
@@ -38,7 +62,6 @@ public class ControladorLivros {
             System.out.print("           ");
             System.out.println(livro.getTitulo());
         }
-        this.abreTela();
     }
 
     public void retornar() {
@@ -46,7 +69,14 @@ public class ControladorLivros {
     }
 
     public void excluirLivro() {
-
+        this.listaLivro();
+        int codigo_livro = this.telaLivro.selecionaLivro();
+        Livro livro = this.pega_livro_por_codigo(codigo_livro);
+        if (livro != null) {
+            this.livros.remove(livro);
+        } else {
+            this.telaLivro.mostraMensagem("Anteção! Livro não existente");
+        }
     }
 
     public void abreTela() {
@@ -57,12 +87,13 @@ public class ControladorLivros {
         listaOpcoes.put(4, this::excluirLivro);
         listaOpcoes.put(0, this::retornar);
 
-        int opcao = this.telaLivro.abreTela();
-
-        if (listaOpcoes.containsKey(opcao)) {
+        while (true) {
+            int opcao = this.telaLivro.abreTela();
+            if (listaOpcoes.containsKey(opcao)) {
             listaOpcoes.get(opcao).run();
         } else {    
             this.abreTela();
+        }
         }
 
     }
