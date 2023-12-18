30552c306d0fd40511a438510dc8f54f3b3cfd0d
Josef Bruseke <josefbruseke@gmail.com>
2023-12-17 21:17:08 -0300

    add changes

--- controler/ControladorEmprestimos.java
+++ controler/ControladorEmprestimos.java
@@ -1,20 +1,72 @@
 package controler;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import model.Emprestimo;
 import view.TelaEmprestimo;
 
 public class ControladorEmprestimos {
     ControladorSistema controladorSistema;
-    TelaEmprestimo telaEmprestimo;
+    private TelaEmprestimo telaEmprestimo;
+    private ArrayList<Emprestimo> emprestimos;
 
     public ControladorEmprestimos(ControladorSistema controladorSistema){
         this.controladorSistema =  controladorSistema;
         this.telaEmprestimo = new TelaEmprestimo();
+        this.emprestimos = new ArrayList<>();
     }
+    
+    public Emprestimo pegaEmprestimoPorCodigo(int codigo) {
+        for (Emprestimo emprestimo: emprestimos){
+            if (emprestimo.getCodigo() == codigo) {
+                return emprestimo;
+            }
+        }
+        return null;
+    }
+
+    public void fazEmprestimo() {
+        this.controladorSistema.getControladorAmigos().listaAmigos();
+        this.controladorSistema.getControladorLivros().listaLivro();
+        dados_emprestimo = this.telaEmprestimo.pegaDadosEmprestimo();
+    }
+
+    public void listaEmprestimos() {
+
+    }
+
+    public void devolveEmprestimo() {
+
+    }
+
+    public void voltar() {
+
+    }
+
+
 
 
     public void abreTela(){
-        this.telaEmprestimo.telaOpcoes();
+        HashMap<Integer, Runnable> listaOpcoes = new HashMap<>();
+        listaOpcoes.put(1, this::fazEmprestimo);
+        listaOpcoes.put(2, this::listaEmprestimos);
+        listaOpcoes.put(3, this::devolveEmprestimo);
+        listaOpcoes.put(0 , this::voltar);
+
+        int opcao = this.telaEmprestimo.telaOpcoes();
+
+        if (listaOpcoes.containsKey(opcao)) {
+            listaOpcoes.get(opcao).run();
+        } else {
+            System.out.println("Opção inválida. Tente novamente.");
+            this.abreTela();
+        }
     }
+
+    
+
+
 
 
 }   
